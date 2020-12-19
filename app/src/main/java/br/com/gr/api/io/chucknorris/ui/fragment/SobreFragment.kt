431c2643a3444b4c484f86ba24fde6a1794e15e7
Gilmar Rodrigues <gilmarrodriguessti@gmail.com>
2020-12-19 17:48:08 -0300

    sobre implementado

--- app/src/main/java/br/com/gr/api/io/chucknorris/ui/fragment/SobreFragment.kt
+++ app/src/main/java/br/com/gr/api/io/chucknorris/ui/fragment/SobreFragment.kt
@@ -4,38 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import br.com.gr.api.io.chucknorris.R
 import br.com.gr.api.io.chucknorris.ui.viewmodel.ComponentesVisuais
 import br.com.gr.api.io.chucknorris.ui.viewmodel.EstadoAppViewModel
-import br.com.gr.api.io.chucknorris.ui.viewmodel.SobreViewModel
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 class SobreFragment : Fragment() {
-
-    private lateinit var notificationsViewModel: SobreViewModel
     private val estadoAppViewModel: EstadoAppViewModel by sharedViewModel()
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(SobreViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_sobre, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_sobre, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        componentesVisuais()
+    }
+
+    private fun componentesVisuais() {
         estadoAppViewModel.temComponentes = ComponentesVisuais(appBar = true, bottomNavigation = true)
     }
 }
